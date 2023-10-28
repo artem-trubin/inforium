2af0633e31e8f31591921474e7d3fbcc0161b257
artem-trubin <artemtrubin.letters@gmail.com>
2023-10-29 00:48:26 +0300

    Added some basic code with test notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
+type Note = {
+  id: string,
+  text: string,
+}
+
+const testNotes: Array<Note> = [
+  {
+    id: "1",
+    text: "Test note",
+  },
+  {
+    id: "2",
+    text: "Another Test Note",
+  },
+  {
+    id: "3",
+    text: "The bestest note out there",
+  },
+];
+
 function App() {
   return (
-    <>
+    <div>
       <div>Hello world!</div>
-    </>
+
+      <ul>
+        {testNotes.map(note => <li>{note.text}</li>)}
+      </ul>
+    </div>
   )
 }
 
