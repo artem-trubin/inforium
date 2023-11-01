2f75e4b94c04ac50064a627fefe6190f5ce5f456
artem-trubin <artemtrubin.letters@gmail.com>
2023-11-01 22:10:22 +0200

    Started work on branching in the editor view

--- src/views/EditorView.tsx
+++ src/views/EditorView.tsx
@@ -1,6 +1,39 @@
+import { useContext } from "react";
+
+import { Context } from "../App";
+import { throwContextError } from "../utils";
+
 const EditorView = () => {
+  const context = useContext(Context);
+
+  if (!context) {
+    throwContextError("EditorView");
+    return null;
+  }
+
+  // switch (block.type) {
+  //   case "note": {
+  //     return <NoteBlockEditor />
+  //   }
+  //   case "todo": {
+  //     return <TodoBlockEditor />
+  //   }
+  // }
+}
+
+const NoteBlockEditor = () => {
   return (
-    <div>This is editor View</div>
+    <div>
+      This is note editor
+    </div>
+  )
+}
+
+const TodoBlockEditor = () => {
+  return (
+    <div>
+      This is a todo editor
+    </div>
   )
 }
 
