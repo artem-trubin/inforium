616bc053d914f8941b0cc18a4a7a72fb292f2a98
artem-trubin <artemtrubin.letters@gmail.com>
2023-12-03 17:39:52 +0200

    Removed testing

--- src/main.tsx
+++ src/main.tsx
@@ -6,16 +6,6 @@ import { BrowserRouter as Router } from 'react-router-dom'
 
 const root = document.getElementById('root');
 
-window.addEventListener('resize', () => {
-  root!.style.background = "red";
-  root!.style.height = window.innerHeight + "px";
-});
-
-window.addEventListener('scroll', () => {
-  root!.style.background = "green";
-  root!.style.height = '100vh';
-});
-
 ReactDOM.createRoot(root!).render(
   <React.StrictMode>
     <Router>
